f570d88723f71c67ae912b53d973222c27d1584e
aburi6800 <aburi6800@gmail.com>
2023-10-07 22:42:28 +0900

    update files.

--- src/asm/define_graphic_chars.asm
+++ src/asm/define_graphic_chars.asm
@@ -1,11 +1,11 @@
 ; license:MIT License
-; copyright-holders:Hiromasa Tanaka
+; copyright-holders:hitoshi Iwai
 
 ; rodata_user
 ; https://github.com/z88dk/z88dk/blob/master/doc/overview.md#a-quick-note-for-asm-code
 ; rodata_user if for constant data
 ; kept in rom if program is in rom
 SECTION rodata_user
-PUBLIC _chars, _colors
+PUBLIC _CHR_PTN_TBL, _CHR_COL_TBL
 
 include "../resources/graphic_chars.asm"
--- src/c/example.c
+++ src/c/example.c
@@ -1,427 +1,443 @@
-// license:MIT License
-// copyright-holders:Hiromasa Tanaka
+// License:MIT License
+// copyright-holders:Hitoshi Iwai
+
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdint.h>
+#include <string.h>
+#include <msx.h>
 #include <msx/gfx.h>
-#include "psgdriver.h"
 
-#define MSX_CLIKSW  0xf3db
-#define MSX_JIFFY   0xfc9e
+
+// 定数（インライン展開される）
 #define MSX_H_TIMI  0xfd9f
-
-#define VRAM_NONE   0x20
 #define VRAM_START  0x1800
 #define VRAM_WIDTH  32
 #define VRAM_HEIGHT 24
+#define CHR_SPACE   0x20
+
+
+// マクロ（インライン展開される）
 #define VPOS(x, y)  (VRAM_START + VRAM_WIDTH * y + x)
 
-// chars.asm::_chars ラベルの参照(VRAM PCG 転送用)
-extern unsigned char chars[];
-// 音楽・効果音のラベル参照（psgdriver.asm 用）
-extern uint8_t music_title[], music_main[], music_game_over[], sound_extend[], sound_get[];
-// サウンドステータス（ワークエリアから取得）
-extern uint8_t sounddrv_bgmwk[];
 
-// ゲームの状態遷移
-typedef enum {
-    TITLE_INIT,
-    TITLE_ADVERTISE,
-    GAME_INIT,
-    GAME_MAIN,
-    GAME_OVER
-} game_state_t;
+// chrptn.asm:CHR_PTN_TBL ラベルの参照(パターンジェネレータテーブル)
+extern uint8_t CHR_PTN_TBL[];
 
-// ゲームの状態
-typedef struct {
-    game_state_t state;
-    uint8_t remein_clear;
-    uint16_t score;
-    uint16_t score_hi;
-    uint8_t stick_state;
-    uint8_t sound_play;
-} game_t;
+// chrptn.asm:CHR_COL_TBL ラベルの参照(カラーテーブル)
+extern uint8_t CHR_COL_TBL[];
 
-game_t game;
 
-// ボールの状態
-typedef struct {
-    uint8_t x;
-    uint8_t y;
-    int8_t vy;
-    uint16_t tick;
-} ball_t;
+// 関数のプロトタイプ宣言
+void write_text(uint8_t x, uint8_t y, char* text);
 
-ball_t ball;
+void add_bcd(uint16_t addvalue, unsigned char* distaddr, uint8_t distbytes);
 
-// タイトルアドバタイズデモの状態
-typedef struct {
-    uint16_t y;
-    int16_t vy;
-    uint16_t tick;
-    uint8_t trigger_state;
-} title_t;
+void write_bcd(unsigned char* buffaddr, uint8_t buffoffset, unsigned char* distaddr, uint8_t distbytes);
 
-title_t title;
+
+// パターンネームテーブル
+uint8_t PTN_NAME_TBL[24*32] = {0};
+
+// スプライトアトリビュートテーブル
+uint8_t SPR_ATTR_TBL[32][4] = {0};
+
+// スコアデータ
+unsigned char score[3] = {0x00, 0x00, 0x00};
+
+// 画面更新完了フラグ
+bool updated = false;
+
 
 /**
- * vsync カウント待ち
+ * 画面更新
  */
-void wait_vsync(uint16_t count)
+void screen_update()
 {
-    uint16_t *interval_timer = (uint16_t *)MSX_JIFFY;
-    *interval_timer = 0;
-    while(*interval_timer < count);
-}
-
-/**
- * グラフィックス初期化
- */
-void init_graphics()
-{
-    // スクリーンモード
-    set_color(15, 1, 1);
-    set_mangled_mode();
-
-    // スプライトモード
-    set_sprite_mode(sprite_default);
-
-    // キークリックスイッチ(OFF)
-    *(uint8_t *)MSX_CLIKSW = 0;
-
-    // 画面クリア
-    fill(VRAM_START, VRAM_NONE, VRAM_WIDTH * VRAM_HEIGHT);
-
-    // PCG 設定(3面に同じデーターを転送)
-    vwrite(chars, 0x0000, 0x800);
-    vwrite(chars, 0x0800, 0x800);
-    vwrite(chars, 0x1000, 0x800);
-
-    // 色設定(0000|0000 = 前|背景)
-    set_char_color('=', 0x54, place_all);
-    set_char_color('$', 0xa0, place_all);
-    set_char_color('>', 0x6d, place_all);
-    set_char_color('?', 0x60, place_all);
-}
-
-/**
- * スコア等表示
- */
-void print_state()
-{
-    uchar score_string[VRAM_WIDTH + 1];
-    sprintf(score_string, "SCORE %06u  HISCORE %06u  %02u", game.score, game.score_hi, game.remein_clear);
-    vwrite(score_string, VPOS(0, 0), VRAM_WIDTH);
-}
-
-/**
- * タイトル表示及びアドバタイズデモ用初期化
- */
-void title_init()
-{
-    // 画面クリア
-    fill(VRAM_START, VRAM_NONE, VRAM_WIDTH * VRAM_HEIGHT);
-
-    game.remein_clear = 0;
-    print_state();
-
-    vwrite("========   =======  ==     ==   ", VPOS(0,  5), VRAM_WIDTH);
-    vwrite("==     == ==     == ===    ==   ", VPOS(0,  6), VRAM_WIDTH);
-    vwrite("==     == ==     == ====   ==   ", VPOS(0,  7), VRAM_WIDTH);
-    vwrite("========  ==     == ==  == ==   ", VPOS(0,  8), VRAM_WIDTH);
-    vwrite("==        ==     == ==   ====   ", VPOS(0,  9), VRAM_WIDTH);
-    vwrite("==         =======  ==     ==   ", VPOS(0, 10), VRAM_WIDTH);
-    vwrite("                                ", VPOS(0, 11), VRAM_WIDTH);
-    vwrite("   ========   =======  ==     ==", VPOS(0, 12), VRAM_WIDTH);
-    vwrite("   ==     == ==     == ===    ==", VPOS(0, 13), VRAM_WIDTH);
-    vwrite("   ==     == ==     == ====   ==", VPOS(0, 14), VRAM_WIDTH);
-    vwrite("   ========  ==     == ==  == ==", VPOS(0, 15), VRAM_WIDTH);
-    vwrite("   ==        ==     == ==   ====", VPOS(0, 16), VRAM_WIDTH);
-    vwrite("   ==         =======  ==     ==", VPOS(0, 17), VRAM_WIDTH);
-    vwrite("          HIT SPACE KEY         ", VPOS(0, 22), VRAM_WIDTH);
-
-    title.y = 6;
-    title.tick = 0;
-    title.vy = 1;
-
-    // サウンド再生
-    sounddrv_bgmplay(music_title);
-
-    // アドバタイズデモに遷移
-    game.state = TITLE_ADVERTISE;
-}
-
-/**
- * タイトルアドバタイズデモ
- */
-void title_advertise(uint8_t trigger)
-{
-    // フレーム間先行入力
-    // ゲームオーバー後の連続入力無視のため最初の 30 tick は入力を受け付けない
-    if(title.tick > 30 && trigger) {
-        title.trigger_state = trigger;
-    } else {
-        title.trigger_state = 0;
+    if (updated)
+    {
+        return;
     }
 
-    // HIT SPACE KEY
-    if(title.trigger_state) {
-        // アドバタイズデモの経過 tick 数で乱数シードを初期化する
-        // ぽんぽん目押しで好きな面を狙える
-        seed_rnd(title.tick);
-        // サウンド停止
-        sounddrv_stop();
-        // ゲーム初期化に遷移
-        game.state = GAME_INIT;
-    }
-
-    // tick per 6
-    if(title.tick++ % 6 != 0) return;
-
-    // ぽんぽん
-    vpoke(VPOS(14, title.y), VRAM_NONE);
-    if(title.y >= 9) title.vy = -1;
-    if(title.y <= 6) title.vy = 1;
-    title.y += title.vy;
-    vpoke(VPOS(14, title.y), '?');
-}
-
-/**
- * ゲーム（ボム及びゴールド配布）
- */
-void game_randam_block(uint8_t count)
-{
-    // クリアまでの個数
-    game.remein_clear = 10;
-    print_state();
-
-    // ゴールドと障害物
-    for(uint8_t i = 0; i < count; i++) {
-        uint8_t x = get_rnd() % 31 + 1;
-        uint8_t y = get_rnd() % 21 + 1;
-        uint16_t add = VPOS(x, y);
-        if(vpeek(add) == VRAM_NONE || vpeek(add) == '>') {
-            if(i % 3) {
-                // ゴールドはボムを潰せる
-                vpoke(add, '$');
-            } else {
-                // 自分の縦移動方向にはボムは配置しない
-                if(ball.x != x) {
-                    vpoke(add, '>');
-                }
-            }
-        }
-    }
-}
-
-/**
- * ゲーム初期化
- */
-void game_init()
-{
-    // 画面クリア
-    fill(VRAM_START, VRAM_NONE, VRAM_WIDTH * VRAM_HEIGHT);
-
-    // 壁
-    fill(VPOS(0,  1) , '=', VRAM_WIDTH);
-    fill(VPOS(0, 22), '=', VRAM_WIDTH);
-    for(uint8_t y = 2; y < 22; y++) {
-        vpoke(VPOS( 0, y), '=');
-        vpoke(VPOS(31, y), '=');
-        if(y == 7 || y == 16) {
-            vwrite("=======      ======      =======", VPOS(0, y), 32);
-        }
-    }
-
-    // ボール（下から上へ）
-    ball.x = 2;
-    ball.y = 6;
-    ball.vy = -1;
-
-    // 初期ゴールドとボム生成
-    game_randam_block(40);
-
-    // ステートクリア
-    ball.tick = 0;
-    game.score = 0;
-    game.stick_state = 0;
-
-    // ステート表示
-    print_state();
-
-    // サウンドステータス初期化
-    game.sound_play = 0;
-
-    // ゲームに遷移
-    game.state = GAME_MAIN;
-}
-
-/**
- * ゲームオーバー
- */
-void game_over(uint8_t trigger)
-{
-    vwrite("=                              =", VPOS(0,  9), VRAM_WIDTH);
-    vwrite("=          GAME OVER           =", VPOS(0, 10), VRAM_WIDTH);
-    vwrite("=                              =", VPOS(0, 11), VRAM_WIDTH);
-    vwrite("=        HIT SPACE KEY         =", VPOS(0, 12), VRAM_WIDTH);
-    vwrite("=                              =", VPOS(0, 13), VRAM_WIDTH);
-
-    // HIT SPACE KEY
-    if(trigger) {
-        // タイトルに遷移
-        game.state = TITLE_INIT;
-    }
-}
-
-/**
- * ゲームメイン
- */
-void game_main(uint8_t stick)
-{
-    uint8_t ball_next_x;
-    uint8_t ball_next_y;
-    int8_t ball_vx = 0;
-
-    // フレーム間先行入力
-    if(stick & st_left || stick & st_right) {
-        game.stick_state = stick;
-    }
-
-    // ファンファーレ再生終わり検知
-    if(game.sound_play == 2 && (sounddrv_bgmwk[3] + sounddrv_bgmwk[4]) == 0) {
-        game.sound_play = 0;
-    }
-    // メインミュージック再生
-    if(game.sound_play == 0) {
-        sounddrv_bgmplay(music_main);
-        // メインミュージック再生中
-        game.sound_play = 1;
-    }
-
-    // tick per 6
-    if(ball.tick++ % 6 != 0) return;
-
-    // ボール移動先算出
-    ball_next_y = ball.y + ball.vy;
-    if(ball.x > 1 && game.stick_state & st_left) ball_vx = -1;
-    if(ball.x < 30 && game.stick_state & st_right) ball_vx += 1;
-    ball_next_x = ball.x + ball_vx;
-
-    // 判定
-    uint8_t next_block = vpeek(VPOS(ball_next_x, ball_next_y));
-    if(next_block == '=') {
-        // 壁反射（ボール的 1 tick 縦移動を停止させる。アイテム破壊猶予）
-        ball_next_y = ball_next_y - ball.vy;
-        ball.vy = ball.vy * -1;
-    } else if(next_block == '$') {
-        // 効果音再生
-        sounddrv_sfxplay(sound_get);
-        // ゴールド取得
-        game.score += 10;
-        if(game.score > game.score_hi) {
-            game.score_hi = game.score;
-        }
-        game.remein_clear--;
-        print_state();
-        // ネクストステージ
-        if(game.remein_clear <= 0) {
-            // ファンファーレ再生中
-            game.sound_play = 2;
-            // ファンファーレ再生
-            sounddrv_bgmplay(sound_extend);
-            // ボム・ゴールド追加
-            game_randam_block(20);
-        }
-    } else if(next_block == '>') {
-        // ボム激突
-        // サウンド停止
-        sounddrv_stop();
-        // サウンド再生
-        sounddrv_bgmplay(music_game_over);
-        // ゲームオーバーに遷移
-        game.state = GAME_OVER;
-    }
-
-    // ボールクリア
-    vpoke(VPOS(ball.x, ball.y), VRAM_NONE);
-    // ボール表示
-    vpoke(VPOS(ball_next_x, ball_next_y), '?');
-
-    // アップデート
-    ball.x = ball_next_x;
-    ball.y = ball_next_y;
-
-    // フレーム間先行入力クリア
-    game.stick_state = 0;
-}
-
-/**
- * ゲームループ及び状態遷移
- */
-void loop()
-{
-    while(1) {
-        // vsync wait
-        wait_vsync(1);
-        // 入力取得
-        uint8_t stick = st_dir[get_stick(0)];
-        uint8_t trigger = get_trigger(0);
-        // ゲーム
-        switch(game.state) {
-            case TITLE_INIT:
-                title_init();
-                break;
-            case TITLE_ADVERTISE:
-                title_advertise(trigger);
-                break;
-            case GAME_INIT:
-                game_init();
-                break;
-            case GAME_MAIN:
-                game_main(stick);
-                break;
-            case GAME_OVER:
-                game_over(trigger);
-                break;
-            default:
-                break;
-        }
-    }
-}
-
-/**
- * メイン
- */
-void main()
-{
-    // 画面初期化
-    init_graphics();
-
-    // サウンドドライバー初期化
-    sounddrv_init();
-    // サウンドドライバーフック設定
-    // __INTELLISENSE__ 判定は vscode で非標準インラインアセンブル構文をエラーにしないように挿入
     #ifndef __INTELLISENSE__
     __asm
     DI
     __endasm;
     #endif
-    uint8_t *h_time = (uint8_t *)MSX_H_TIMI;
-    uint16_t hook = (uint16_t)sounddrv_exec;
-    h_time[0] = 0xc3; // JP
-    h_time[1] = (uint8_t)(hook & 0xff);
-    h_time[2] = (uint8_t)((hook & 0xff00) >> 8);
+
+    // パターンネームテーブル更新
+    vwrite(PTN_NAME_TBL, 0x1800, 768);
+
+    // スプライトアトリビュートテーブル更新
+    vwrite(SPR_ATTR_TBL, 0x1B00, 128);
+
     #ifndef __INTELLISENSE__
     __asm
     EI
     __endasm;
     #endif
 
-    // ゲームステート初期化
-    game.state = TITLE_INIT;
-    game.score_hi = 300;
+    updated = true;
+    return;
+}
 
-    // start
-    loop();
+
+/**
+ * テキスト表示
+ * - 構造体に指定されたテキストを仮想画面のx,y座標に書き込む。
+ * - 画面端のはみ出し処理はしていないので注意。
+ *
+ * args:
+ * - text_t         text        テキスト表示関数の構造体
+ *
+ * return:
+ * - void
+ */
+//void write_text(write_text_t arg)
+void write_text(uint8_t x, uint8_t y, char* text)
+{
+    for (uint8_t i = 0; i < strlen(text); i++)
+    {
+        PTN_NAME_TBL[y*32+x] = text[i];
+        x++;
+    }
+}
+
+
+/**
+ * BCD加算
+ * - 対象データに対し引数で与えた16ビットのBCD値を加算する。
+ *
+ * args:
+ * - addvalue       uint16_t    加算値
+ * - distaddr       char        加算対象データアドレス
+ * - distbytes      uint8_t     加算対象データバイト数
+ *
+ * return:
+ * - void
+ */
+void add_bcd(uint16_t addvalue, unsigned char* distaddr, uint8_t distbytes)
+{
+    #ifndef __INTELLISENSE__
+__asm
+    // リターンアドレスをスキップ
+    LD HL,2
+    ADD HL,SP                   // SP+2を引数取得開始アドレスに設定
+
+    // 第3引数(加算対象データバイト数)を取得
+    LD B,(HL)                   // B <- 加算対象データバイト数
+    INC HL
+    INC HL
+
+    // 第2引数(加算対象データアドレス)を取得
+    LD E,(HL)
+    INC HL
+    LD D,(HL)
+    INC HL
+    PUSH DE                     // スタックに退避
+
+    // 第1引数(加算値)を取得
+    LD E,(HL)                   // DE <- 加算値
+    INC HL
+    LD D,(HL)
+
+    // レジスタ設定
+    POP HL                      // HL <- 加算対象データアドレス
+
+    // 加算対象データバイト数により処理開始アドレス設定
+    DEC B                       // 加算対象データバイト数を1減算(1byte=0、2byte=1、3byte=2)
+
+    LD A,B                      // A <- B
+    ADD A,L                     // HL=HL+A
+    JR NC,add_bcd_L1
+    INC H
+
+add_bcd_L1:
+    LD L,A
+
+    // 1〜2桁目の加算
+	LD A,E					    // AレジスタにEレジスタの値をロード
+    ADD A,(HL)			        // Aレジスタの値に(HL)の値を加算
+    DAA						    // Aレジスタの値を内部10進に補正
+    						    // 桁溢れした場合はここでキャリーフラグが立つ
+    PUSH AF                     // フラグの状態を保存
+    LD (HL),A				    // Aレジスタの値を(HL)に格納
+
+    LD A,B                      // Aレジスタに現在の加算対象データバイト数をロード
+    OR A                        // ゼロか？
+    JR Z,add_bcd_L2             // ゼロならadd_bcd_L2へ
+    DEC A                       // 加算対象データバイト数を1減算
+    LD B,A                      // Bレジスタに保存
+
+    // 3〜4桁目の加算
+    DEC HL                      // 加算対象データアドレスを-1
+    POP AF                      // フラグの状態を復帰
+    LD A,D					    // AレジスタにDレジスタの値をロード
+    ADC A,(HL)			        // Aレジスタの値に(HL)＋キャリーフラグを加算
+    						    // 桁溢れした場合はキャリーフラグが立つが無視する
+    DAA						    // Aレジスタの値を内部10進に補正
+    PUSH AF                     // フラグの状態を保存
+    LD (HL),A				    // Aレジスタの値を(HL)に格納
+
+    JR NC,add_bcd_L2            // 桁繰り上がりが無ければadd_bcd_L2へ
+
+    LD A,B                      // Aレジスタに現在の加算対象データバイト数をロード
+    OR A                        // ゼロか？
+    JR Z,add_bcd_L2             // ゼロならadd_bcd_L2へ
+
+    // 5〜6桁目の加算（桁繰り上がり発生時のみ）
+    POP AF                      // フラグの状態を復帰
+    OR A                        // フラグクリア
+    DEC HL                      // 加算対象データアドレスを-1
+    LD A,(HL)                   // 5〜6桁目に1加算
+    INC A
+    DAA                         // Aレジスタの値を内部10進に補正
+    PUSH AF                     // フラグの状態を保存（ダミー）
+    LD (HL),A                   // Aレジスタの値を(HL)に格納
+
+add_bcd_L2:
+    POP AF
+
+__endasm
+    #endif
+}
+
+
+/**
+ * BCD値仮想画面表示
+ *
+ * args:
+ * - buffaddr       char        仮想画面アドレス
+ * - buffoffset     uint8_t     仮想画面オフセット値
+ * - distaddr       char        表示データアドレス
+ * - distbytes      uint8_t     表示データバイト数
+ *
+ * return:
+ * - void
+ */
+void write_bcd(unsigned char* buffaddr, uint8_t buffoffset, unsigned char* distaddr, uint8_t distbytes)
+{
+    #ifndef __INTELLISENSE__
+__asm
+    // リターンアドレスをスキップ
+    LD HL,2
+    ADD HL,SP                   // SP+2を引数取得開始アドレスに設定
+
+    // 第4引数(表示データバイト数)を取得
+    LD B,(HL)                   // B -< 表示データバイト数
+    INC HL
+    INC HL
+
+    // 第3引数(表示データアドレス)を取得
+    LD E,(HL)
+    INC HL
+    LD D,(HL)
+    INC HL
+    PUSH DE                     // スタックに退避
+
+    // 第2引数(仮想画面オフセット値)を取得
+    LD E,(HL)                   // DE <- 仮想画面オフセット値
+    INC HL
+    LD D,(HL)
+    INC HL
+    PUSH DE                     // スタックに退避
+
+    // 第1引数(仮想画面アドレス)を取得
+    LD E,(HL)                   // DE <- 仮想画面アドレス
+    INC HL
+    LD D,(HL)
+    PUSH DE                     // スタックに退避
+
+    // レジスタ設定
+    POP HL                      // HL <- 仮想画面アドレス
+    POP DE                      // HL <- 仮想画面オフセット値
+    ADD HL,DE                   // HL(仮想画面オフセット値)にDE(仮想画面アドレス)を加算 = 更新対象の仮想画面先頭アドレスを求める
+
+    POP DE                      // DE <- 表示データアドレス
+
+PRTBCD:
+    // 表示する桁数分、スペースで埋める
+    PUSH BC
+    PUSH DE
+    PUSH HL
+
+    LD A,B
+    ADD A,A                     // 表示文字数を算出 (表示桁*2)
+    LD B,A                      // B(繰り返し数) = 表示文字数
+
+PRTBCD_L2:
+    LD (HL),$20                 // 表示桁分、スペースを埋める
+    INC HL
+    DJNZ PRTBCD_L2
+
+    DEC HL
+    LD (HL),$30                 // 末尾はゼロ固定表示
+
+    POP HL
+    POP DE
+    POP BC
+
+    // 数値描画
+    LD C,0                      // ゼロ表示フラグ初期化
+
+PRTBCD_L3:
+    LD A,(DE)                   // A <- DE(表示データ)
+	CALL PUTBCD 			    // データを表示
+
+	INC DE					    // DE=DE+1(＝表示データの次のアドレスが設定される)
+    INC HL					    // HL=HL+1(＝表示位置を1つ右に移動)
+
+    DJNZ PRTBCD_L3              // B=B-1、ゼロでなければ繰り返す
+
+	RET
+
+    // 1バイト(2桁)のBCD値描画
+PUTBCD:
+	// 上1桁の処理
+    PUSH AF
+    SRL A					    // Aレジスタの値を4回右シフトして、上位4ビットを取り出す
+    SRL A
+    SRL A
+    SRL A
+    CALL PUTBCD_L1			    // オフスクリーンバッファにデータ設定
+
+	// 下1桁の処理
+    POP AF
+    INC HL					    // HLレジスタの値を1加算(＝データ表示位置を1つ右に移動)
+
+PUTBCD_L1:
+	// 仮想画面にデータ設定
+	AND $0F				        // 上位4ビットをゼロにする(=下位4ビットの値だけ取り出す)
+    OR A
+    JR NZ,PUTBCD_L2             // 値がゼロ以外の場合はL2へ
+
+    INC C                       // 値がゼロの時はゼロ表示フラグを判定
+    DEC C
+    RET Z                       // ゼロ表示フラグがOFFの時はゼロ表示せず抜ける
+
+PUTBCD_L2:
+    INC C                       // 以降のゼロは表示させるので、ゼロ表示フラグを+1
+    ADD A,$30				    // 値にキャラクタコード&H30('0')を加える
+    LD (HL),A                   // 仮想画面にデータを設定
+
+    RET
+
+__endasm
+    #endif
+}
+
+
+/**
+ * 初期設定処理
+ *
+ * args:
+ * - none
+ *
+ * return:
+ * - void
+ */
+void game_init()
+{
+    // H.TIMIフック設定
+    #ifndef __INTELLISENSE__
+    __asm
+    DI
+    __endasm;
+    #endif
+    uint8_t *h_timi = (uint8_t *)MSX_H_TIMI;
+    uint16_t hook_addr = (uint16_t)&screen_update;
+    h_timi[0] = 0xc3; // JP
+    h_timi[1] = (uint8_t)(hook_addr & 0xff);
+    h_timi[2] = (uint8_t)((hook_addr & 0xff00) >> 8);
+    #ifndef __INTELLISENSE__
+    __asm
+    EI
+    __endasm;
+    #endif
+
+    // 画面初期化
+    set_color(15, 1, 1);
+    set_mangled_mode();
+
+    // パターンジェネレータテーブル設定
+    vwrite(CHR_PTN_TBL, 0x0000, 0x0800);
+    vwrite(CHR_PTN_TBL, 0x0800, 0x0800);
+    vwrite(CHR_PTN_TBL, 0x1000, 0x0800);
+
+    // カラーテーブル設定
+    vwrite(CHR_COL_TBL, 0x2000, 0x0800);
+    vwrite(CHR_COL_TBL, 0x2800, 0x0800);
+    vwrite(CHR_COL_TBL, 0x3000, 0x0800);
+
+    // 画面クリア
+    fill(VRAM_START, CHR_SPACE, VRAM_WIDTH * VRAM_HEIGHT);
+}
+
+
+/**
+ * 画面描画
+ */
+void make_screen()
+{
+    write_text(0, 0, "faaf                        aafa");
+    write_text(4, 0, "babfaaabaaaaafbfaafaaaab");
+    write_text(0, 1, "aafd                        ceba");
+    write_text(0, 2, "bff                          aaf");
+    write_text(0, 3, "fac                          daf");
+    write_text(0, 4, "ad                            fa");
+    write_text(0, 5, "a d                           ca");
+    write_text(0, 6, "a                            e a");
+    write_text(0, 7, "d                              e");
+    write_text(0, 8, " d                            e ");
+    write_text(0, 9, "b                               ");
+    write_text(0,10, "f                              b");
+    write_text(0,11, "bd                            bf");
+    write_text(0,12, "d                             eb");
+    write_text(0,13, "                               e");
+    write_text(0,20, "gggggggggggggggggggggggggggggggg");
+    write_text(0,22, " SCORE        ROUND      LEFT"); //:GOSUB 2400
+//8160 IF GS=1 THEN 8220
+    write_text(0,15, "hh");
+    write_text(0,16, "hhh");
+    write_text(0,17, "ooh");
+    write_text(0,18, "  h");
+    write_text(0,19, "  h");
+//8220 IF GS=5 THEN RETURN
+    write_text(29,15, " hh");
+    write_text(29,16, "hhh");
+    write_text(29,17, "hoo");
+    write_text(29,18, "hof");
+    write_text(29,19, "hfo");
+}
+
+
+/**
+ * ゲーム初期化
+ */
+void init_screen()
+{
+    // 画面表示
+    make_screen();
+}
+
+
+/**
+ * メイン
+ */
+int main()
+{
+    // ゲーム初期設定
+    game_init();
+
+    // 画面初期化
+    init_screen();
+
+    while(true) {
+        if (updated)
+        {
+            updated = false;
+
+            add_bcd(0x0010, score, sizeof(score));
+            write_bcd(PTN_NAME_TBL, 6+(22*32), score, sizeof(score));
+        }
+    }
 }
